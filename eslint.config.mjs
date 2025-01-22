b4077870a22ed31627c2f9cf7efca8abc40067b3
Wilfredo <jrlopez85@gmail.com>
2025-01-22 16:27:43 -0500

    Next js setup with prettier, tailwind and lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,10 +7,56 @@ const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: { root: true }, // Fixed the missing parameter
+  ignorePatterns: ["components/ui/**"],
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      rules: {
+        "no-undef": "off",
+      },
+    },
+  ],
+  rules: {
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin", // Built-in types are first
+          "external", // External libraries
+          "internal", // Internal modules
+          ["parent", "sibling"], // Parent and sibling types can be mingled together
+          "index", // Then the index file
+          "object", // Object imports
+        ],
+        newlinesBetween: "always",
+        pathGroups: [
+          {
+            pattern: "@app/**",
+            group: "external",
+            position: "after",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["builtin"],
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "eslint:recommended",
+    "standard",
+    "plugin:tailwindcss/recommended",
+    "prettier"
+  ),
+  ...compat.plugins("import", "promise", "n"),
 ];
 
 export default eslintConfig;
