722848fddbf9a4f2b18d7e6f700710abd4c163ae
Wilfredo <jrlopez85@gmail.com>
2025-02-12 19:29:26 -0500

    Implement working tag cards and editor to ask a question page

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,8 +1,15 @@
+import QuestionForm from '@/components/form/QuestionForm'
 import React from 'react'
 
 const AskQuestion = () => {
   return (
-    <div>AskAQuestion</div>
+    <>
+      <h1 className='h1-bold text-dark100_light900'>Ask a Question</h1>
+
+      <div className='mt-9'>
+        <QuestionForm />        
+      </div>
+    </>
   )
 }
 
--- components/cards/TagCard.tsx
+++ components/cards/TagCard.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import ROUTES from '@/constants/routes'
 import { Badge } from "../ui/badge";
 import { getDeviconClassName } from '@/lib/utils';
+import Image from 'next/image';
 
 interface Props {
   _id: string;
@@ -10,29 +11,58 @@ interface Props {
   questions?: number;
   showCount?: boolean;
   compact?: boolean;
+  remove?: boolean;
+  isButton?: boolean;
+  handleRemove: () => void
 }
 
-const TagCard = ({ _id, name, questions, showCount, compact }: Props) => {
+const TagCard = ({ _id, name, questions, showCount, compact, remove, isButton, handleRemove }: Props) => {
   
   const iconClass = getDeviconClassName(name)
-  
-  return (
 
-    <Link 
-      href={ROUTES.TAGS(_id)} 
-      className='flex justify-between gap-2'
-    >
-      <Badge className='subtle-medium background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase'>
+  const handleClick = (e: React.MouseEvent) => {
+    e.preventDefault()
+  }
+
+  const Content = (
+    <>
+      <Badge className='subtle-medium background-light800_dark300 text-light400_light500 flex flex-row gap-2 rounded-md border-none px-4 py-2 uppercase'>
         <div className='flex-center space-x-2'>
           <i className={`${iconClass} text-sm`}></i>
           <span>{name}</span>
         </div>
+
+        {remove && (
+          <Image 
+            src="/icons/close.svg"
+            alt="Close"
+            width={12}
+            height={12}
+            className='cursor-pointer object-contain invert-0 dark:invert'
+            onClick={handleRemove}
+          />
+        )}
       </Badge>
       {showCount && (
         <p className='small-medium text-dark500_light700'>{questions}</p>
       )}
-    </Link>
+    </>
   )
+
+  if(compact) {
+    return isButton ? (
+      <button onClick={handleClick} className='flex justify-between gap-2'>
+        {Content}
+      </button>
+    ) : (
+      <Link 
+        href={ROUTES.TAGS(_id)} 
+        className='flex justify-between gap-2'
+      >
+        {Content}
+      </Link>
+    )
+  }
 }
 
 export default TagCard
