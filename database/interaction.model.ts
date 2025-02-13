0b55ff066f7aac49ac5fc6f22d72474586bf9b22
Wilfredo <jrlopez85@gmail.com>
2025-02-13 18:46:14 -0500

    Implemented collection, interaction and vote model for MongoDB/Mongoose

---
+++ database/interaction.model.ts
@@ -0,0 +1,22 @@
+import { model, models, Schema, Types } from "mongoose";
+import { string } from "zod";
+
+export interface IInteraction {
+  user: Types.ObjectId
+  action: string
+  actionId: Types.ObjectId
+  actionType: "question" | "answer"
+}
+
+const InteractionSchema = new Schema<IInteraction>(
+  {
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    action: {string, required: true},
+    actionId: { type: Schema.Types.ObjectId, required: true },
+    actionType: { type: String, enum: ["question", "answer"], required: true },
+  }, { timestamps: true }
+);
+
+const Interaction = models?.Interaction || model<IInteraction>("Interaction", InteractionSchema);
+
+export default Interaction;
