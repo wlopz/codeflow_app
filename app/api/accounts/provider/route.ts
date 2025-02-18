dbf8438b7419583ba80bb86b319436b3394a42dc
Wilfredo <jrlopez85@gmail.com>
2025-02-18 14:27:47 -0500

    Implemented account by provider route

---
+++ app/api/accounts/provider/route.ts
@@ -0,0 +1,25 @@
+import Account from "@/database/account.model"
+import handleError from "@/lib/handlers/error"
+import { NotFoundError, ValidationError } from "@/lib/http-errors"
+import { AccountSchema } from "@/lib/validations"
+import { NextResponse } from "next/server"
+
+export async function POST(request: Request) {
+  const { providerAccountId } = await request.json()
+
+  try {
+    const validatedData = AccountSchema.partial().safeParse({ providerAccountId })
+
+    if(!validatedData.success) throw new ValidationError(validatedData.error.flatten().fieldErrors)
+    
+    const account = await Account.findOne({ providerAccountId })
+
+    if(!account) throw new NotFoundError("Account not found")
+
+    return NextResponse.json({ success: true, data: account }, { status: 200 })
+    
+  } catch (error) {
+    // eslint-disable-next-line no-undef
+    return handleError(error, "api") as APIErrorResponse
+  }
+}
