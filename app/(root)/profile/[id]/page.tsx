228104d1356defab90f00fb95927ed07ccc3ce14
Wilfredo <jrlopez85@gmail.com>
2025-04-28 18:48:16 -0500

    Implemented tabs layout in profile page

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -5,6 +5,7 @@ import React from "react";
 
 import { auth } from "@/auth";
 import { Button } from "@/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import ProfileLink from "@/components/user/ProfileLink";
 import Stats from "@/components/user/Stats";
 import UserAvatar from "@/components/UserAvatar";
@@ -35,14 +36,14 @@ const Profile = async ({ params }: RouteParams) => {
     <>
       <section className="flex flex-col-reverse items-start justify-between sm:flex-row">
         <div className="flex flex-col items-start gap-4 lg:flex-row">
-          <UserAvatar id={_id} name={name} imageUrl={image} />
+          <UserAvatar id={_id} name={name} imageUrl={image} className="size-[140px] rounded-full object-cover" fallbackClassName="text-6xl font-bolder" />
 
           <div className="mt-3">
             <h2 className="h2-bold text-dark100_light900">{name}</h2>
             <p className="paragraph-regular text-dark200_light800">
               @{username}
             </p>
-            <div className="mt-5 flex-wrap items-center justify-start gap-5">
+            <div className="mt-5 flex flex-wrap items-center justify-start gap-5">
               {portfolio && (
                 <ProfileLink
                   imgUrl="/icons/link.svg"
@@ -83,6 +84,35 @@ const Profile = async ({ params }: RouteParams) => {
         totalAnswers={totalAnswers}
         badges={{ GOLD: 0, SILVER: 0, BRONZE: 0 }}
       />
+
+      <section className="mt-10 flex gap-10">
+        <Tabs defaultValue="top-posts" className="flex-[2]">
+          <TabsList className="background-light800_dark400 min-h-[42px] p-1">
+            <TabsTrigger value="top-posts" className="tab">
+              Top Posts
+            </TabsTrigger>
+            <TabsTrigger value="answers" className="tab">
+              Answers
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent
+            value="top-posts"
+            className="mt-5 flex w-full flex-col gap-6"
+          >
+            List of Questions
+          </TabsContent>
+          <TabsContent value="answers" className="flex w-full flex-col gap-6">
+            List of Answers
+          </TabsContent>
+        </Tabs>
+
+        <div className="flex w-full min-w-[250px] flex-1 flex-col max-lg:hidden">
+          <h3 className="h3-bold text-dark200_light900">Top Tech</h3>
+          <div className="mt-7 flex flex-col gap-4">
+            <p>List of Tags</p>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
